ae7b9bade33591b6f34628177773697957a6bb8d
Whitesmith <whitesmith.thedj@gmail.com>
2021-03-22 22:36:32 +0100

    Fin TP12 TOB

--- TOB/tps/TP12/MorpionSwing.java
+++ TOB/tps/TP12/MorpionSwing.java
@@ -84,9 +84,68 @@ public class MorpionSwing {
 		// Construire le contrôleur (gestion des événements)
 		this.fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		initialiserInterface();
+
 		// afficher la fenêtre
 		this.fenetre.pack();			// redimmensionner la fenêtre
 		this.fenetre.setVisible(true);	// l'afficher
+	}
+
+	private void initialiserInterface() {
+		final JPanel gridPanel = new JPanel();
+		final GridLayout layout = new GridLayout(3, 3);
+		gridPanel.setLayout(layout);
+		for (int i = 0; i < this.cases.length; i++) {
+			for (int j = 0; j < this.cases[i].length; j++) {
+				gridPanel.add(this.cases[i][j]);
+				int finalI = i, finalJ = j;
+				this.cases[i][j].addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						try {
+							ModeleMorpion.Etat joueur = modele.getJoueur();
+							modele.cocher(finalI, finalJ);
+							cases[finalI][finalJ].setIcon(images.get(joueur));
+							if (modele.estTerminee()) {
+								if (modele.estGagnee())
+									JOptionPane.showMessageDialog(fenetre, joueur.toString() + " a Gagné!");
+								else
+									JOptionPane.showMessageDialog(fenetre, "Personne n'a gagné!");
+							}
+						} catch (CaseOccupeeException caseOccupeeException) {
+						}
+					}
+				});
+				this.cases[i][j].setIcon(images.get(ModeleMorpion.Etat.VIDE));
+			}
+		}
+
+		// Menu
+		JMenuBar mb = new JMenuBar();
+		JMenu menu = new JMenu("Jeu");
+		JMenuItem n, q;
+		n = new JMenuItem("Nouvelle partie");
+		q = new JMenuItem("Quitter");
+		menu.add(n);
+		menu.add(q);
+		mb.add(menu);
+		fenetre.setJMenuBar(mb);
+
+		// Listeners
+		n.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				recommencer();
+			}
+		});
+		q.addActionListener(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				quitter();
+			}
+		});
+
+		this.fenetre.getContentPane().add(gridPanel);
 	}
 
 // Quelques réactions aux interactions de l'utilisateur
@@ -107,7 +166,9 @@ public class MorpionSwing {
 		joueur.setIcon(images.get(modele.getJoueur()));
 	}
 
+	public void quitter() {
 
+	}
 
 // La méthode principale
 // ---------------------
